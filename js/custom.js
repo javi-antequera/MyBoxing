efa0534b2cbf4887481ab34400cb7f4ef4615a21
Javier Antequera <javiantmine@gmail.com>
2023-02-27 19:27:22 +0100

    añadiendo y retocando

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,5 @@
-$(document).ready(function() {
-  // Selecciona todos los enlaces de la barra de navegación
-  $('a.nav-link').on('click', function(event) {
-    // Asegura que el enlace tenga un hash válido y no sea el mismo que el de la página actual
-    if (this.hash !== "" && this.hash !== "#home") {
-      // Previene el comportamiento predeterminado del enlace
-      event.preventDefault();
-      // Almacena el hash en una variable
-      let hash = this.hash;
-      // Usa jQuery para animar el desplazamiento suave al elemento con el hash correspondiente
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        // Agrega el hash al URL después de la animación (para que el usuario pueda ver la ubicación en la que se desplazó)
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+//Funcion que valida el formulario
 function validarForm(){
-
   let nombre = document.getElementById("nombre").value; 
   let mail = document.getElementById("email").value;
   let textarea = document.getElementById("textarea").value;
@@ -26,7 +7,7 @@ function validarForm(){
   let prohibidos = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   
   if ( !prohibidos.test(mail) ){                                                         
-      alert("Error: La dirección de correo " + correo + " es incorrecta.");
+      alert("Error: La dirección de correo " + mail + " es incorrecta.");
   }
   
   if ((nombre == " ") || (mail == " ") || (textarea == " ")) { 
@@ -34,3 +15,26 @@ function validarForm(){
   }
   
   }
+
+  //Funcion para el zoom en las imagenes
+  $(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+//Función paera que al pulsar un elemento del navbar tenga una animación con un desplazamiento suave
+$(document).ready(function() {
+  $('a.nav-link').on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      let hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
